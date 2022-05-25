93df6c6fef357d2271b1d19063f0b79f659aea68
Yasin Khan <yasinkhan500@gmail.com>
2022-05-25 16:22:15 -0700

    stache

--- connection.js
+++ connection.js
@@ -1,16 +1,17 @@
-const {Sequelize} = require("sequelize");
-const sequelize = new Sequelize("sqlite::memory:");
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
 const Trades = require('./models/trades');
 
-return sequelize.authenticate()
-    .then(result => {
-        console.log(`SQLite successfully connected!`);
-        return Trades.sync();
-    })
-    .then(result => {
-        console.log(`Trades table created`);
-        return result;
-    })
-    .catch(error => {
-        console.error('Unable to connect to SQLite database:', error);
-    })
+return sequelize
+	.authenticate()
+	.then(result => {
+		console.log(`SQLite successfully connected!`);
+		return Trades.sync();
+	})
+	.then(result => {
+		console.log(`Trades table created`);
+		return result;
+	})
+	.catch(error => {
+		console.error('Unable to connect to SQLite database:', error);
+	});
